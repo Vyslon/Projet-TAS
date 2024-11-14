683847470aa2172f7b418ad3d633661099c9b81c
Vyslon <santoni.thomas26@hotmail.fr>
2024-11-14 21:57:54 +0100

    Évaluation de liste implémentée

--- main.ml
+++ main.ml
@@ -154,7 +154,8 @@ let rec ltr_ctb_step (t : pterm) : pterm option =
     | None -> let nextXS = (ltr_ctb_step xs) in
         match nextXS with
         | Some reductionXS -> Some (Cons(x, reductionXS))
-        | None -> Some (Cons(x, xs))
+        (* | None -> Some (Cons(x, xs)) *)
+        | None -> None
 
 
 (* Appelle consécutivement ltr_ctb_step, pour normaliser un terme autant que possible *)
@@ -309,6 +310,7 @@ let print_inference_result term env typeAttendu =
   (* Création de listes pour les tests *)
   let liste1 = Cons (Entier 1, Cons (Entier 2, Cons (Entier 3, Nil))) in
   let liste2 = Cons (Entier 4, liste1) in  (* [4, 1, 2, 3] *)
+  let liste3 = Cons (Addition (Entier 1, Entier 2), Cons (Entier 2, Cons ((Soustraction (Entier 8, Entier 2)), Nil))) in
   let liste_vide = Nil in
 
   (* Définition de la limite de carburant pour éviter les boucles infinies *)
@@ -325,6 +327,12 @@ let print_inference_result term env typeAttendu =
   (match ltr_cbv_norm' liste2 fuel with
    | Some result -> Printf.printf "Résultat: %s\n\n" (print_term result)
    | None -> Printf.printf "Échec de l'évaluation ou divergence pour liste2\n\n");
+
+    (* Test de l'évaluation de liste3 *)
+  Printf.printf "Évaluation de liste3: %s\n" (print_term liste3);
+  (match ltr_cbv_norm' liste3 fuel with
+   | Some result -> Printf.printf "Résultat: %s\n\n" (print_term result)
+   | None -> Printf.printf "Échec de l'évaluation ou divergence pour liste3\n\n");
 
   (* Test de l'évaluation d'une liste vide *)
   Printf.printf "Évaluation de liste_vide: %s\n" (print_term liste_vide);
